8d7d5e6dfa2429b396716ed2692c8f3518674050
albertos <alberto.nieto.peg@upm.es>
2021-06-10 15:53:08 +0200

    version1

--- src/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-import { CustomButton as CustomButtonComponent } from './components/CustomButton'
-
-export const CustomButton = props => <CustomButtonComponent {...props} /> 
